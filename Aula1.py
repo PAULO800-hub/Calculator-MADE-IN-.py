be9d3f0c2cd2a6b03cf8b676d7dad7284fd0013f
Paulo Natan <paulo.dev.codigo@gmail.com>
2024-09-08 15:00:58 -0300

    SECOND COMMIT

---
+++ Aula1.py
@@ -0,0 +1,15 @@
+ 
+
+""""
+DocString nao e um comentario
+
+ Escrever o que eu quiser asdfghjkl
+
+"""
+
+''' Usar para escrever suas notas '''
+
+# Permite escrever um comentário
+print(123)  #Na Frente print(456) <esta ingnorando
+# Abaixo
+print(456)
